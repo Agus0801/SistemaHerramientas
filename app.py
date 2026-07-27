7743cdf6cbc162ac902fb9b20fe48cc156bda144
Agus0801 <agustinadorinlopez@gmail.com>
2026-07-27 12:02:07 -0300

    Configurar puerto para Render

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from routes.herramientas import herramientas_bp
 from routes.electricistas import electricistas_bp
 from routes.prestamos import prestamos_bp
 from routes.historial import historial_bp
+import os
 
 app = Flask(__name__)
 
@@ -27,5 +28,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
-
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
